fbb69ebdde87776690994e73087195fc5733ca94
Diego Cartelle <diegocartelle@gmail.com>
2022-02-15 00:38:21 +1100

    ej 3

--- tarea m3 u4 part2/script.js
+++ tarea m3 u4 part2/script.js
@@ -7,60 +7,17 @@ function contadorText(obj){
     document.getElementById('textNum').innerHTML = obj.value.length+ ' characters';
 }
 
-var alumnos = [
+const alumnos = [{nombre: 'Juan Gomez', nota: 7},
+                {nombre: 'Pedro Rodriguez', nota: 4},
+                {nombre: 'Roxana García', nota: 8},
+                {nombre: 'Luciano Lopez', nota: 5},
+                {nombre: 'Fernanda Gimenez', nota: 6},
+                {nombre: 'Florencia Martinez', nota: 10},
+                {nombre: 'Raul Sanchez', nota: 7},
+                {nombre: 'Sandra Figueroa', nota: 8}
+              ]
 
-    {
-  
-      nombre: 'Juan Gomez',
-  
-      nota: 7
-  
-    }, {
-  
-      nombre: 'Pedro Rodriguez',
-  
-      nota: 4
-  
-    }, {
-  
-      nombre: 'Roxana García',
-  
-      nota: 8
-  
-    }, {
-  
-      nombre: 'Luciano Lopez',
-  
-      nota: 5
-  
-    }, {
-  
-      nombre: 'Fernanda Gimenez',
-  
-      nota: 6
-  
-    }, {
-  
-      nombre: 'Florencia Martinez',
-  
-      nota: 10
-  
-    }, {
-  
-      nombre: 'Raul Sanchez',
-  
-      nota: 7
-  
-    }, {
-  
-      nombre: 'Sandra Figueroa',
-  
-      nota: 8
-  
-    }
-  
-  ];
 
-  function alumnosAprobados(){
-      
-  }
+let aprovados = alumnos.filter(alumno => alumno.nota >= 7);
+
+console.log(aprovados);
