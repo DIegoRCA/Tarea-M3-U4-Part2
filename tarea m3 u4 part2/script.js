ed863e1e318c6b4c474e042e99c0fd1cf759aedf
DIegoRCA <48502715+DIegoRCA@users.noreply.github.com>
2022-02-14 13:45:25 +1100

    ultima version ej 1y 2

--- tarea m3 u4 part2/script.js
+++ tarea m3 u4 part2/script.js
@@ -2,3 +2,65 @@ function changeColor(y){
     var x = document.getElementById("color");
     x.style.backgroundColor= y
 }
+
+function contadorText(obj){
+    document.getElementById('textNum').innerHTML = obj.value.length+ ' characters';
+}
+
+var alumnos = [
+
+    {
+  
+      nombre: 'Juan Gomez',
+  
+      nota: 7
+  
+    }, {
+  
+      nombre: 'Pedro Rodriguez',
+  
+      nota: 4
+  
+    }, {
+  
+      nombre: 'Roxana García',
+  
+      nota: 8
+  
+    }, {
+  
+      nombre: 'Luciano Lopez',
+  
+      nota: 5
+  
+    }, {
+  
+      nombre: 'Fernanda Gimenez',
+  
+      nota: 6
+  
+    }, {
+  
+      nombre: 'Florencia Martinez',
+  
+      nota: 10
+  
+    }, {
+  
+      nombre: 'Raul Sanchez',
+  
+      nota: 7
+  
+    }, {
+  
+      nombre: 'Sandra Figueroa',
+  
+      nota: 8
+  
+    }
+  
+  ];
+
+  function alumnosAprobados(){
+      
+  }
